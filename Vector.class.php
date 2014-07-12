8fc6234b567b631de0e047d1857e71efd9fc475f
Sidney Harrell <sidney@eventespresso.com>
2014-07-12 19:55:58 -0400

    added equality test and cast any strings in constructor argument to either floats or ints

--- Vector.class.php
+++ Vector.class.php
@@ -11,6 +11,14 @@ class Vector
 			$this->index = 0;
 			foreach ($initial_components as $initial_component) {
 				if ( is_numeric( $initial_component ) ) {
+					if (is_string( $initial_component )) {
+						$int_test = (int) $initial_component;
+						if ( $int_test == $initial_component ) {
+							$initial_component = (int) $initial_component;
+						} else {
+							$initial_component = (float) $initial_component;
+						}
+					}
 					$this->components[$this->index] = $initial_component;
 				}
 				$this->index++;
@@ -18,4 +26,32 @@ class Vector
 			$this->index = 0;
 		}
     }
+	
+	/**
+	 * 
+	 * @param Vector vector to be compared to $this vector
+	 * @return boolean returns false if vector's components are not equal and of the same type
+	 */
+	
+	public function testEquality (Vector $other_vector) {
+		$other_components = $other_vector->getComponents();
+		if ( is_array( $other_components ) && count($other_components) === count($this->components) ) {
+			$this->index = 0;
+			foreach ($other_components as $other_component) {
+				if ( !is_numeric( $other_component ) || $this->components[$this->index] !== $other_component ) {
+					return FALSE;
+				}
+				$this->index++;
+			}
+			$this->index = 0;
+			return TRUE;
+		} else {
+			return FALSE;
+		}
+		
+	}
+	
+	public function getComponents() {
+		return $this->components;
+	}
 }
--- sample_client_code.php
+++ sample_client_code.php
@@ -1,5 +1,11 @@
 <?php
-
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 include_once 'Vector.class.php';
-$vector1 = new Vector(array('1','2'));
-var_dump($vector1);
+$vector1 = new Vector(array('1', 2));
+$vector2 = new Vector(array('1', '2'));
+$vector3 = new Vector(array('1', '3'));
+$test = $vector1->testEquality($vector2);
+var_dump($test);
+$test = $vector1->testEquality($vector3);
+var_dump($test);
